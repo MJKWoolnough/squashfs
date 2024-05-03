e598008f1dc6b986f151c8471587f0c0f7fd0cff
Michael Woolnough <michael.woolnough@gmail.com>
2024-05-03 20:19:30 +0100

    pull out 'getReader' method to enable reuse

--- file.go
+++ file.go
@@ -29,71 +29,12 @@ func (f *file) Read(p []byte) (int, error) {
 	}
 
 	if f.reader == nil {
-		if f.block < len(f.file.blockSizes) {
-			start := int64(f.file.blocksStart)
-
-			for _, size := range f.file.blockSizes[:f.block] {
-				start += int64(size & 0xeffffff)
-			}
-
-			var err error
-			size := int64(f.file.blockSizes[f.block])
-			if size&(1<<24) == 0 {
-				if f.reader, err = f.squashfs.superblock.Compressor.decompress(io.NewSectionReader(f.squashfs.reader, start, size)); err != nil {
-					return 0, err
-				}
-			} else {
-				f.reader = io.NewSectionReader(f.squashfs.reader, start, size&0xeffffff)
-			}
-		} else if f.file.fragIndex != 0xFFFFFFFF {
-			ler := byteio.LittleEndianReader{Reader: io.NewSectionReader(f.squashfs.reader, int64(f.squashfs.superblock.FragTable)+int64(f.file.fragIndex>>10), 8)}
-
-			mdPos, _, err := ler.ReadUint64()
-			if err != nil {
-				return 0, err
-			}
-
-			r, err := f.squashfs.readMetadata((uint64(f.file.fragIndex)<<4)%8192, mdPos)
-			if err != nil {
-				return 0, err
-			}
-
-			ler = byteio.LittleEndianReader{Reader: r}
-
-			start, _, err := ler.ReadUint64()
-			if err != nil {
-				return 0, err
-			}
-
-			size, _, err := ler.ReadUint32()
-			if err != nil {
-				return 0, err
-			}
-
-			if unused, _, err := ler.ReadUint32(); err != nil {
-				return 0, err
-			} else if unused != 0 {
-				return 0, fs.ErrInvalid
-			}
-
-			fragmentSize := int64(f.file.fileSize) % int64(f.squashfs.superblock.BlockSize)
-
-			if size&(1<<24) == 0 {
-				if f.reader, err = f.squashfs.superblock.Compressor.decompress(io.NewSectionReader(f.squashfs.reader, int64(start), int64(size))); err != nil {
-					return 0, err
-				}
-
-				if err := skip(f.reader, int64(f.file.blockOffset)); err != nil {
-					return 0, err
-				}
-
-				f.reader = io.LimitReader(f.reader, fragmentSize)
-			} else {
-				f.reader = io.NewSectionReader(f.squashfs.reader, int64(start)+int64(f.file.blockOffset), fragmentSize)
-			}
-		} else {
-			return 0, io.EOF
+		reader, err := f.getReader(f.block)
+		if err != nil {
+			return 0, err
 		}
+
+		f.reader = reader
 	}
 
 	if f.skip > 0 {
@@ -118,6 +59,72 @@ func (f *file) Read(p []byte) (int, error) {
 	}
 
 	return n, err
+}
+
+func (f *file) getReader(block int) (io.Reader, error) {
+	if block < len(f.file.blockSizes) {
+		start := int64(f.file.blocksStart)
+
+		for _, size := range f.file.blockSizes[:block] {
+			start += int64(size & 0xeffffff)
+		}
+
+		size := int64(f.file.blockSizes[block])
+		if size&(1<<24) == 0 {
+			return f.squashfs.superblock.Compressor.decompress(io.NewSectionReader(f.squashfs.reader, start, size))
+		}
+
+		return io.NewSectionReader(f.squashfs.reader, start, size&0xeffffff), nil
+	} else if f.file.fragIndex != 0xFFFFFFFF {
+		ler := byteio.LittleEndianReader{Reader: io.NewSectionReader(f.squashfs.reader, int64(f.squashfs.superblock.FragTable)+int64(f.file.fragIndex>>10), 8)}
+
+		mdPos, _, err := ler.ReadUint64()
+		if err != nil {
+			return nil, err
+		}
+
+		r, err := f.squashfs.readMetadata((uint64(f.file.fragIndex)<<4)%8192, mdPos)
+		if err != nil {
+			return nil, err
+		}
+
+		ler = byteio.LittleEndianReader{Reader: r}
+
+		start, _, err := ler.ReadUint64()
+		if err != nil {
+			return nil, err
+		}
+
+		size, _, err := ler.ReadUint32()
+		if err != nil {
+			return nil, err
+		}
+
+		if unused, _, err := ler.ReadUint32(); err != nil {
+			return nil, err
+		} else if unused != 0 {
+			return nil, fs.ErrInvalid
+		}
+
+		fragmentSize := int64(f.file.fileSize) % int64(f.squashfs.superblock.BlockSize)
+
+		if size&(1<<24) == 0 {
+			reader, err := f.squashfs.superblock.Compressor.decompress(io.NewSectionReader(f.squashfs.reader, int64(start), int64(size)))
+			if err != nil {
+				return nil, err
+			}
+
+			if err := skip(reader, int64(f.file.blockOffset)); err != nil {
+				return nil, err
+			}
+
+			return io.LimitReader(reader, fragmentSize), nil
+		}
+
+		return io.NewSectionReader(f.squashfs.reader, int64(start)+int64(f.file.blockOffset), fragmentSize), nil
+	}
+
+	return nil, io.EOF
 }
 
 func (f *file) Seek(offset int64, whence int) (int64, error) {
