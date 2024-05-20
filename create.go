80792763f5f61bf5347fff58c2caf54e98c2ce8a
Michael Woolnough <michael.woolnough@gmail.com>
2024-05-20 19:01:29 +0100

    allow setting of options on creator

--- create.go
+++ create.go
@@ -4,16 +4,33 @@ import (
 	"io"
 )
 
+const (
+	defaultBlockSize = 1 << 17 // 128K
+)
+
+type Option func(*superblock) error
+
 type Creater struct {
 	writer     io.WriterAt
 	superblock superblock
 }
 
-func Create(w io.WriterAt) (*Creater, error) {
-	return &Creater{
-		writer: w,
-		superblock: superblock{
-			CompressionOptions: defaultGzipOptions(),
+func Create(w io.WriterAt, options ...Option) (*Creater, error) {
+	s := superblock{
+		Stats: Stats{
+			BlockSize: defaultBlockSize,
 		},
+		CompressionOptions: defaultGzipOptions(),
+	}
+
+	for _, o := range options {
+		if err := o(&s); err != nil {
+			return nil, err
+		}
+	}
+
+	return &Creater{
+		writer:     w,
+		superblock: s,
 	}, nil
 }
