8cbdaa7d52ec291f139bb5d289dec050f6635c6f
Michael Woolnough <michael.woolnough@gmail.com>
2024-05-20 19:03:16 +0100

    add Option for setting BlockSize

--- create.go
+++ create.go
@@ -2,13 +2,27 @@ package squashfs
 
 import (
 	"io"
+	"math/bits"
 )
 
 const (
+	minBlockSize     = 1 << 12 // 4K
 	defaultBlockSize = 1 << 17 // 128K
+	maxBlockSize     = 1 << 20 // 1MB
 )
 
 type Option func(*superblock) error
+
+func BlockSize(blockSize uint32) Option {
+	return func(s *superblock) error {
+		if blockSize < minBlockSize || blockSize > maxBlockSize || bits.OnesCount32(blockSize) != 1 {
+			return ErrInvalidBlockSize
+		}
+
+		s.BlockSize = blockSize
+		return nil
+	}
+}
 
 type Creater struct {
 	writer     io.WriterAt
