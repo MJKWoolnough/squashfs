007eaf55e3cdfee5580ae509b7c44ea84148b887
Michael Woolnough <michael.woolnough@gmail.com>
2024-05-30 21:07:26 +0100

    extended block cache tests to test invalidation

--- blockcache_test.go
+++ blockcache_test.go
@@ -33,13 +33,29 @@ func TestBlockCache(t *testing.T) {
 		if err != nil {
 			t.Fatalf("unexpected error: %s", err)
 		} else if num := readBlock(f); num != byte(i)%10 {
-			t.Errorf("test %d.1: expecting to read byte %d, got %d", i+1, i%10, num)
+			t.Errorf("test 1.%d.1: expecting to read byte %d, got %d", i+1, i%10, num)
 		} else if i < 10 {
 			if f, err := b.getBlock(int64(i), nil, CompressorGZIP); err != nil {
 				t.Fatalf("unexpected error: %s", err)
 			} else if num := readBlock(f); num != byte(i) {
-				t.Errorf("test %d.1: expecting to read byte %d, got %d", i+1, i, num)
+				t.Errorf("test 1.%d.1: expecting to read byte %d, got %d", i+1, i, num)
 			}
+		}
+	}
+
+	for i := 0; i < 10; i++ {
+		f, err := b.getBlock(int64(i+10), compress(i+10), CompressorGZIP)
+		if err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		} else if num := readBlock(f); num != byte(i+10) {
+			t.Errorf("test 2.%d.1: expecting to read byte %d, got %d", i+1, i+10, num)
+		}
+
+		f, err = b.getBlock(int64(i), compress(i+10), CompressorGZIP)
+		if err != nil {
+			t.Fatalf("unexpected error: %s", err)
+		} else if num := readBlock(f); num != byte(i+10) {
+			t.Errorf("test 2.%d.2: expecting to read byte %d, got %d", i+1, i+10, num)
 		}
 	}
 }
